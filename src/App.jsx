627e05ecdc81496bf0580aa8a0d76cf25f3fce01
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-11-05 12:43:46 +0500

    I restyled this project

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,53 @@ import React from 'react'
 import { useQuery} from "@tanstack/react-query";
 
 function App() {
+    const { isLoading, error, data } = useQuery({
+        queryKey: ['repoData'],
+        queryFn: () =>
+            fetch('https://jsonplaceholder.typicode.com/users').then((res) =>
+                res.json(),
+            ),
+    });
 
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['repoData'],
-    queryFn: () =>
-        fetch('https://api.github.com/repos/TanStack/query').then((res) =>
-            res.json(),
-        ),
-  });
+    if (isLoading) return 'Loading...';
 
-  if (isLoading) return 'Loading...';
+    if (error) return 'An error has occurred: ' + error.message;
 
-  if (error) return 'An error has occurred: ' + error.message;
 
-  return (
-    <div className="app">
+    return (
+        <div className="app">
 
-      <button onClick={()=> {console.log(data)}}>
-        click me
-      </button>
+            <button onClick={()=> {console.log(data)}}>
+                click me
+            </button>
 
-      <h1>{data.name}</h1>
-      <p>{data.description}</p>
-      <strong>👀 {data.subscribers_count}</strong>{' '}
-      <strong>✨ {data.stargazers_count}</strong>{' '}
-      <strong>🍴 {data.forks_count}</strong>
+            <br/><br/>
 
-    </div>
-  );
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-xl-7 border rounded p-3">
+                        <table className="table table-bordered">
+                            <tbody>
+                                {
+                                    data.map((user, index) => {
+                                        return(
+                                            <tr>
+                                                <td>{index + 1}</td>
+                                                <td>{user.name}</td>
+                                                <td>{user.email}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+
 };
 
 export default App
